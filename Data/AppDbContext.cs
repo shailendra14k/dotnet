43808bfe558fcaa724e8d3e1abe0ef2cb56caeed
Shailendra Kumar Singh <shsingh@redhat.com>
2024-03-01 16:01:06 +0530

    initial

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace CoreSqlLite.Data
+{
+    using CoreSqlLite.Models;
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+
+}
---
+++ Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreSqlLite.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+}
